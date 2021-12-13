32ef66a329f5b68abf121f3749a9f3e42052ea66
PatChizzy <patchizzymba@gmail.com>
2021-12-13 18:29:10 +0100

    Assignment 2

---
+++ flask-app/README.md
@@ -0,0 +1,1 @@
+# This Repo contains all assignments carried out for `MLOps` knowledge acquisition
---
+++ flask-app/app/BMI_calculator.py
@@ -0,0 +1,6 @@
+def bmi_result(weight, height):
+
+    """Function to calculate BMI given height and weight as parameters"""
+
+    bmi = weight / (height ** 2)
+    return bmi
